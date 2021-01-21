6a5489a841d57a8b79cd34cfaef146956123db89
Aansulaimann <aansulaiman92@gmail.com>
2021-01-21 11:59:43 +0700

    modified navbar, add some image and text in main and add new folder img in public

--- index.php
+++ index.php
@@ -7,35 +7,45 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
    <link rel="preconnect" href="https://fonts.gstatic.com">
    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;600&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
     
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light mt-3">
-            <div class="container">
-                <a class="navbar-brand" href="#"><b>CV Generator</b></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ms-auto">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Pricing</a>
-                        <a class="nav-link btn btn-primary ms-4 px-4 text-white" href="#">Create CV</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+   <header>
+      <nav class="navbar navbar-expand-lg navbar-light mt-3">
+         <div class="container">
+               <a class="navbar-brand" href="#"><b>CV Generator</b></a>
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav ms-auto">
+                     <a class="nav-link active me-4" aria-current="page" href="#">Home</a>
+                     <a class="nav-link me-4" href="#">Features</a>
+                     <a class="nav-link me-4" href="#">Pricing</a>
+                     <a class="btn btn-outline-warning ms-4 px-4" href="#">Create CV</a>
+                  </div>
+               </div>
+         </div>
+      </nav>
+   </header>
 
-    <main>
+   <main>
       <section>
-         <div class="bg-main">
-
+         <div class="container">
+            <div class="row">
+               <div class="col-12 col-lg-6">
+                  <img src="public/img/sapiens.svg" alt="404" class="img-fluid">
+               </div>
+               <div class="col-12 text-center col-lg-6 align-self-center main-headlines">
+                  <h3>let's <span>create</span> your own span <span>amazing</span> cv</h4>
+                  <p>make your cv now!</p>
+                  <button class="btn btn-warning text-white px-5 py-2 mt-3">Create CV</button>
+               </div>
+            </div>
          </div>
       </section>
-    </main>
+   </main>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
